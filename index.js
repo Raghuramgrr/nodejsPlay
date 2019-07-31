f823854408e9987525b88b0e0de56125981b3c3c
raghuramgrr <raghuramgrr@gmail.com>
2019-08-01 00:41:58 +0800

    Simple Node Js Insert form Data

---
+++ index.js
@@ -0,0 +1,58 @@
+var express    = require('express');
+var app        = express();
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(app.router);
+app.use(express.static('public'));
+
+
+var connection = mysql.createConnection({
+
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'nodedb'
+
+});
+
+//connection.connect();
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Fuck you!");
+});
+
+
+app.get('/', function (req, res) {
+
+    res.sendFile(__dirname + '/index.html');
+
+});
+
+
+app.post('/data', function(req, res){
+
+    var username=req.body.name;
+    console.log(username)
+    var sql = "INSERT INTO `names`(`names`) VALUES ('"+req.body.name+"')";
+
+    connection.query(sql , function(err, result){
+        if(err) throw err;
+        console.log("Fuck you again");
+    });
+
+    res.send(username);
+
+});
+
+//connection.end();
+
+app.listen(3000, function () {
+console.log('Raghus app listening on port 3000');
+});
